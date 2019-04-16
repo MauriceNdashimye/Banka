17af8468c821cbc9e963f68498d903442dc1a592
Maurice Ndashimye <mauricendashimye@gmail.com>
2019-04-16 09:13:41 +0200

    changes

--- api/models/account.js
+++ api/models/account.js
@@ -1,15 +1,14 @@
 import mongoose from 'mongoose';
+import { strict } from 'assert';
 
 const accountModel = mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
-  email: String,
-  phoneNumber: Number,
-  idNumber: Number,
-  firstName: { type:String },
-  lastName: String,
-  password: String,
-  type: String, 
-  isAdmin: Boolean,
+_id: mongoose.Schema.Types.ObjectId,
+accountNumber: Number,
+createdOn : String,
+owner :  mongoose.Schema.Types.ObjectId, 
+type : String , // savings, current
+status : String, // draft, active, or dormant
+balance : String
 });
 
 module.exports = mongoose.model('Account', accountModel)
